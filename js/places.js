08ef0883576bab01dcb6811be07379e57206aee5
kokocrater <scott.jochim@att.net>
2016-06-20 22:30:29 -0500

    json validated

--- js/places.js
+++ js/places.js
@@ -1,21 +1,21 @@
-{"coast": [
-	{"location":"Oregon Dunes National Recreation Area", "state": "OR"},
-	{"location":"Neahkahnie Mountain", "state": "OR"},
-	{"location":"Sea Lion Caves", "state": "OR"},
-	{"location":"Heceta Head Lighthouse", "state": "OR"},
-	{"location":"Undersea Gardens", "state": "OR"}
+{"coast":[
+	{"location":"Oregon Dunes National Recreation Area", "state":"OR"},
+	{"location":"Neahkahnie Mountain", "state":"OR"},
+	{"location":"Sea Lion Caves", "state":"OR"},
+	{"location":"Heceta Head Lighthouse", "state":"OR"},
+	{"location":"Undersea Gardens", "state":"OR"}
 ]}
 
-{"mountains": [
-	{"location": "Mt Shasta", "state": "CA"},
-	{"location": "Mt McLoughlin", "state": "OR"},
-	{"location": "The Three Sisters Wilderness", "state": "OR"},
-	{"location": "Mt Hood", "state": "OR"}
-	{"location": "Hood River Valley", "state": "OR"}
+{"mountains":[
+	{"location": "Mt Shasta", "state":"CA"},
+	{"location": "Mt McLoughlin", "state":"OR"},
+	{"location": "The Three Sisters Wilderness", "state":"OR"},
+	{"location": "Mt Hood", "state":"OR"},
+	{"location": "Hood River Valley", "state":"OR"}
 ]}
 
-{"desert": [
-	{"location": "Joshua Tree National Park", "state": "CA"},
-	{"location": "Smith Rock State Park", "state": "OR"},
-	{"location": "Monument Valley", "state": "AZ"}
+{"desert":[
+	{"location": "Joshua Tree National Park", "state":"CA"},
+	{"location": "Smith Rock State Park", "state":"OR"},
+	{"location": "Monument Valley", "state":"AZ"}
 ]}
